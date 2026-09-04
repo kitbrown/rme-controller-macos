9443ac39a961788a91bc00baeb0b0d4cb496381b
kitbrown <108029357+kitbrown@users.noreply.github.com>
2026-09-04 12:46:49 -0400

    Handle CommonJS dependencies in Stream Deck bundle

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,4 @@
+import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 
@@ -9,8 +10,9 @@ export default {
     sourcemap: true
   },
   plugins: [
-    resolve({ preferBuiltins: true }),
-    typescript({ tsconfig: "./tsconfig.json" })
+    typescript({ tsconfig: "./tsconfig.json" }),
+    resolve({ browser: false, exportConditions: ["node"], preferBuiltins: true }),
+    commonjs()
   ],
   external: [/^node:/]
 };
