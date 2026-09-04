ea3fec8a4d9454fe241f500e7e6d1bfe2d47bcdb
kitbrown <108029357+kitbrown@users.noreply.github.com>
2026-09-04 11:33:24 -0400

    Add OSC configuration

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+import type { JsonObject } from "@elgato/streamdeck";
+
+export const DEFAULT_HOST = "127.0.0.1";
+export const DEFAULT_SEND_PORT = 7008;
+export const DEFAULT_RECEIVE_PORT = 9008;
+
+export function cfg(settings: JsonObject) {
+  return {
+    host: typeof settings.host === "string" ? settings.host : DEFAULT_HOST,
+    sendPort: typeof settings.sendPort === "number" ? settings.sendPort : DEFAULT_SEND_PORT,
+    receivePort: typeof settings.receivePort === "number" ? settings.receivePort : DEFAULT_RECEIVE_PORT
+  };
+}
